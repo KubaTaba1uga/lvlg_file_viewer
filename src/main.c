63351f6a96ac5740743d19fa4471c7b288e331a1
KubaTaba1uga <luna7337anul@proton.me>
2025-07-27 13:26:20 +0200

    add minimal working example

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
-#define _POSIX_C_SOURCE 199309L
-
-#include "lvgl.h"
 #include <stdint.h>
 #include <time.h>
+#include <unistd.h>
+
+#include "lvgl.h"
 
 uint32_t my_get_millis(void) {
   struct timespec ts;
@@ -16,46 +16,20 @@ int main(void) {
   lv_init();
 
   lv_tick_set_cb(my_get_millis);
-  lv_display_t *disp = lv_x11_window_create("LVGL X11 Simulation", 200, 600);
 
+  /* Create X11 driver */
+  lv_display_t *disp = lv_x11_window_create("LVGL X11 Simulation", 200, 600);
   lv_x11_inputs_create(disp, NULL);
 
-  /* lv_display_t *display = lv_display_create(320, 240); */
+  /* Create widgets */
+  lv_obj_t *label = lv_label_create(lv_screen_active());
+  lv_label_set_text(label, "Hello LVGL!");
 
-  /* /\*LVGL will render to this 1/10 screen sized buffer for 2 bytes/pixel*\/
-   */
-  /* static uint8_t buf[320 * 240 / 10 * 2]; */
-  /* lv_display_set_buffers(display, buf, NULL, LV_DISPLAY_RENDER_MODE_PARTIAL);
-   */
-
-  /* /\*This callback will display the rendered image*\/ */
-
-  /* /\*Create widgets*\/ */
-  /* lv_obj_t *label = lv_label_create(lv_screen_active()); */
-  /* lv_label_set_text(label, "Hello LVGL!"); */
-
-  /* /\*Make LVGL periodically execute its tasks*\/ */
-  /* while (1) { */
-  /*   lv_timer_handler(); */
-  /*   my_sleep(5); /\*Wait 5 milliseconds before processing LVGL timer again*\/
-   */
-  /* } */
+  /* Make LVGL periodically execute its tasks */
+  while (1) {
+    lv_timer_handler();
+    usleep(5); /*Wait 5 milliseconds before processing LVGL timer again*/
+  }
 
   return 0;
 }
-
-/* /\*Return the elapsed milliseconds since startup. */
-/*  *It needs to be implemented by the user*\/ */
-/* uint32_t my_get_millis(void) { return my_tick_ms; } */
-
-/* /\*Copy the rendered image to the screen. */
-/*  *It needs to be implemented by the user*\/ */
-/* void my_flush_cb(lv_display_t *disp, const lv_area_t *area, uint8_t *px_buf)
- * { */
-/*   /\*Show the rendered image on the display*\/ */
-/*   my_display_update(area, px_buf); */
-
-/*   /\*Indicate that the buffer is available. */
-/*    *If DMA were used, call in the DMA complete interrupt*\/ */
-/*   lv_display_flush_ready(); */
-/* } */
